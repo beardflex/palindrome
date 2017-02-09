879b28a5ed9ca04b86b34d28efcdddd6e60f6907
David Roche <ardeith69er@gmail.com>
2017-02-09 22:39:45 +0000

    Updated TestPalindrome.java to reflect new spacing and commenting/Javadoc.

--- src/test/java/com/github/beardflex/TestPalindrome.java
+++ src/test/java/com/github/beardflex/TestPalindrome.java
@@ -6,56 +6,54 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+/** Test class to perform functional unit tests on methods provided by {@link Palindrome} class.
+ ** @author David Roche
+ **/
 public class TestPalindrome {
-	
-	private Palindrome palindrome;
-	
-	/** Setup method for testing. Creates a {@link Palindrome} object for use by test methods.
-	 **/
-	@Before
-	public void setUp(){
-		palindrome = new Palindrome();
-	}
-	
-	/** Tear down method for testing. Assigns a null value to the {@link Palindrome} object created
-	 ** in the {@link TestPalindrome#setUp} method.
-	 **/
-	@After
-	public void tearDown(){
-		palindrome = null;
-	}
-	/** Functional test for the {@link Palindrome#getCombinations} method. <br>
-	 ** 
-	 **/
-	@Test
-	public void testGenerateCombinations() {
-		assertTrue(true);
-	}
-	/** Functional test for the {@link Palindrome#isPalindrome} method. <br>
-	 ** Verifies that the method will return true for known even and odd length palindromes.
-	 **/
-	@Test
-	public void testIsPalindrome() {
-		String validOddPalindrome = "Kayak";
-		String validEvenPalindrome = "Abba";
-		
-		assertTrue(palindrome.isPalindrome(validOddPalindrome));
-		assertTrue(palindrome.isPalindrome(validEvenPalindrome));
-	}
-	/** Functional test for the {@link Palindrome#isPalindrome} method. <br>
-	 ** Verifies that the method will return false for known even and odd length words that are not palindromes.
-	 **/
-	@Test
-	public void testIsNotPalindrome() {
-		String invalidOddPalindrome = "Hello";
-		String invalidEvenPalindrome = "Worlds";
-		
-		assertFalse(palindrome.isPalindrome(invalidOddPalindrome));
-		assertFalse(palindrome.isPalindrome(invalidEvenPalindrome));
-	}
-	
-	@Test
-	public void testAssessmentQuestion() {
-		assertTrue(true);
-	}
+    // The member wide Palindrome instance used by all tests below.
+    private Palindrome palindrome;
+    
+    /** Setup method for testing. Creates a {@link Palindrome} object for use by test methods.
+     **/
+    @Before
+    public void setUp(){
+        palindrome = new Palindrome();
+    }
+    
+    /** Tear down method for testing. Assigns a null value to the {@link Palindrome} object created
+     ** in the {@link TestPalindrome#setUp} method.
+     **/
+    @After
+    public void tearDown(){
+        palindrome = null;
+    }
+    /** Functional test for the {@link Palindrome#getCombinations} method. <br>
+     ** 
+     **/
+    @Test
+    public void testGenerateCombinations() {
+        assertTrue(true);
+    }
+    /** Functional test for the {@link Palindrome#isPalindrome} method. <br>
+     ** Verifies that the method will return true for known even and odd length palindromes.
+     **/
+    @Test
+    public void testIsPalindrome() {
+        String validOddPalindrome = "Kayak";
+        String validEvenPalindrome = "Abba";
+        
+        assertTrue(palindrome.isPalindrome(validOddPalindrome));
+        assertTrue(palindrome.isPalindrome(validEvenPalindrome));
+    }
+    /** Functional test for the {@link Palindrome#isPalindrome} method. <br>
+     ** Verifies that the method will return false for known even and odd length words that are not palindromes.
+     **/
+    @Test
+    public void testIsNotPalindrome() {
+        String invalidOddPalindrome = "Hello";
+        String invalidEvenPalindrome = "Worlds";
+        
+        assertFalse(palindrome.isPalindrome(invalidOddPalindrome));
+        assertFalse(palindrome.isPalindrome(invalidEvenPalindrome));
+    }
 }
