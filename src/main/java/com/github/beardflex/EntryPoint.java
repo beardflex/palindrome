4b47263ba37bb79ddb9930517d5ef907d49acbc4
David Roche <ardeith69er@gmail.com>
2017-02-09 16:28:56 +0000

    * Updated the 'getCombinations' method in 'Palindrome.java'.\n * Added the '--show-combinations' cli switch.

--- src/main/java/com/github/beardflex/EntryPoint.java
+++ src/main/java/com/github/beardflex/EntryPoint.java
@@ -1,38 +1,61 @@
 package com.github.beardflex;
 
-import java.util.Set;
+import java.util.List;
+import java.util.ArrayList;
 
 public class EntryPoint {
+	
+	private static boolean showCombinations = false;
+	private static final String STR_Show_Combinations_Switch = "--show-combinations";
 	
 	public static void main(String[] args) {
 		// If no arguments are supplied, print out the usage.
 		if(args.length == 0) {
 			usage();
 		} else {
+			// Check to see if the 'showCombinations' switch was set and return the rest of the args.
+			String[] programArgs = getArgs(args);
+			
 			// Create the Palindrome object.
 			Palindrome palindrome = new Palindrome();
-			// Generate the Set of combinations of the passed in words.
-			Set<String> combinations = palindrome.getCombinations(args);
+			// Generate the List of combinations of the passed in words.
+			List<String> combos = palindrome.getCombinations(programArgs);
+			
 			// Print out the original passed in words along with their combinations.
-			System.out.printf("Received %d words. Words: \n", args.length);
-			for(String word : args) {
+			System.out.printf("Received %d words. Words: \n", programArgs.length);
+			for(String word : programArgs) {
 				System.out.printf("\t * %s\n", word);
 			}
-			System.out.printf("Computed combinations: \n");
-			for(String combo : combinations) {
-				System.out.printf("\t * %s\n", combo);
+			System.out.printf("Computed %d combinations.\n", combos.size());
+			if(showCombinations) {
+				System.out.printf("Combinations: \n");
+				for(String combo : combos) {
+					System.out.printf("\t * %s\n", combo);
+				}
 			}
 			// Now print out which of these words are palindromes.
 			System.out.printf("Among these words the palindromes are: \n");
-			for(String combo : combinations) {
+			for(String combo : combos) {
 				if(palindrome.isPalindrome(combo))
 					System.out.printf("\t * %s\n", combo);
 			}
 		}
 	}
 	
+	public static String[] getArgs(String[] args) {
+		List<String> argList = new ArrayList<String>();
+		for(String arg : args) {
+			if(arg.equals(STR_Show_Combinations_Switch)) {
+				showCombinations = true;
+			} else {
+				argList.add(arg);
+			}
+		}
+		return argList.toArray(new String[argList.size()]);
+	}
+	
 	public static void usage() {
 		System.out.printf("Usage: \n");
-		System.out.printf("palindrome \"first_word\" \"second_word\" ... \"nth_word\"\n");
+		System.out.printf("palindrome \"first_word\" \"second_word\" ... \"nth_word\" [--show-combinations]\n");
 	}
 }
--- src/main/java/com/github/beardflex/Palindrome.java
+++ src/main/java/com/github/beardflex/Palindrome.java
@@ -1,25 +1,47 @@
 package com.github.beardflex;
 
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.List;
+import java.util.Collections;
 
 public class Palindrome {
 	
-	public Set<String> getCombinations(String[] names) {
-		// Create a Set to store the combinations. By using a set, we won't get any duplicate entries.
-		Set<String> combinations = new HashSet<String>();
-		// Iterate through all the combinations of names, excluding where both the first and second names are the same.
-		for (String firstName : names) {
-			for(String secondName : names) {
-				if(!firstName.equals(secondName)) {
-					combinations.add(String.join("", firstName, secondName));
-				}
-			}
-		}
-		return combinations;
-	}
+	public List<String> getCombinations(String[] words) {
+		// Use a Set internally so we don't have any duplicate entries.
+        Set<String> distinctCombinations = new HashSet<String>();
+        // For each word, assign that word as a prefix and use the remaining words to generate combinations.
+        for(String word : words) {
+            String prefix = word;
+            List<String> remaining = new ArrayList<String>(Arrays.asList(words));
+            remaining.remove(word);
+            recurse(prefix, remaining, distinctCombinations);
+        }
+        // Sort the combinations using length, and then alphabetical.
+		List<String> sortedCombinations = new ArrayList<String>(distinctCombinations);
+		Collections.sort(sortedCombinations, (String x1, String x2) -> {
+				if(x1.length() < x2.length())
+					return -1;
+				else if (x1.length() == x2.length())
+					return x1.compareTo(x2);
+				else 
+					return 1;
+			});
+        return sortedCombinations;
+    }
+	public void recurse (String prefix, List<String> remaining, Set<String> combinations) {
+		// For each remaining word, append it to the passed in prefix, add it to the Set, remove that word
+		// from the pool of words and recurse the function.
+        for(String word : remaining) {
+            String tempPrefix = String.join("", prefix, word);
+            combinations.add(tempPrefix);
+            List<String> tempRemaining = new ArrayList<String>(remaining);
+            tempRemaining.remove(word);
+            recurse(tempPrefix, tempRemaining, combinations);
+        }
+    }
 	
 	public boolean isPalindrome(String word) {
 		 // Make sure all chars in the String are in the same case.
